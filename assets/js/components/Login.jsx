cb9d7707d482b0cce415faab83eebced735f9edd
luqiwang <wangluqi1001@gmail.com>
2018-04-18 15:21:31 -0400

    login page style

--- assets/js/components/Login.jsx
+++ assets/js/components/Login.jsx
@@ -3,13 +3,18 @@ import CoinList from './CoinList'
 
 export default function Login () {
   return (
-    <div>
-      <a href="/auth/google" className="btn btn-block btn-social btn-google">
-        <span className="fa fa-google"></span> Sign in with Google
-      </a>
-      <a href="/auth/facebook" className="btn btn-block btn-social btn-facebook">
-        <span className="fa fa-facebook"></span> Sign in with Facebook
-      </a>
+    <div id="login-container">
+      <div className="jumbotron" style={{fontSize:30, backgroundImage: "url(" + "images/jumbotron.jpg" + ")"}}>
+        <span style={{color:'grey'}}>CryptoMonitor</span>
+      </div>
+      <div>
+        <a href="/auth/google" className="btn btn-block btn-social btn-google">
+          <div className="fa fa-google"></div> Sign in with Google
+        </a>
+        <a href="/auth/facebook" className="btn btn-block btn-social btn-facebook">
+          <span className="fa fa-facebook"></span> Sign in with Facebook
+        </a>
+      </div>
     </div>
   )
 }
